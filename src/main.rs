c151acfaeeba8526a79b0833c7d2a3bf87811736
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2026-03-13 23:59:08 +0200

    loop for rotating cube

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use crossterm::{
     ExecutableCommand, QueueableCommand, cursor,
+    event::{self, Event},
     style::{self, Stylize},
     terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use std::io::{self, Write};
+use std::time::Duration;
 
 mod cube;
 mod position;
@@ -13,30 +15,40 @@ use crate::cube::Cube;
 fn main() -> io::Result<()> {
     let width = 20;
     let height = 10;
+    let mut quit = false;
 
     let mut cube = Cube::new();
-
     let mut stdout = io::stdout();
 
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(cursor::Hide)?;
-    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+    terminal::enable_raw_mode()?;
+    while !quit {
+        if event::poll(Duration::from_millis(100))? {
+            if let Event::Key(_) = event::read()? {
+                quit = true;
+            }
+        }
 
-    cube.apply_xz_rotation(20.0_f32.to_radians());
+        stdout.execute(terminal::Clear(terminal::ClearType::All))?;
 
-    for corner in cube.corners {
-        let position2d = corner.transform_position_to_2d();
-        let calculated_position = position2d.position_in_terminal_scale(width, height);
-        stdout
-            .queue(cursor::MoveTo(
-                calculated_position.x as u16 + 2,
-                calculated_position.y as u16 + 2,
-            ))?
-            .queue(style::PrintStyledContent("▆".magenta()))?;
+        cube.apply_xz_rotation(5.0_f32.to_radians());
+
+        for corner in &cube.corners {
+            let position2d = corner.transform_position_to_2d();
+            let calculated_position = position2d.position_in_terminal_scale(width, height);
+            stdout
+                .queue(cursor::MoveTo(
+                    calculated_position.x as u16 + 2,
+                    calculated_position.y as u16 + 2,
+                ))?
+                .queue(style::PrintStyledContent("▆".magenta()))?;
+        }
+        stdout.flush()?;
+        std::thread::sleep(Duration::from_millis(10));
     }
-    stdout.flush()?;
-    std::thread::sleep(std::time::Duration::from_secs(3));
 
+    terminal::disable_raw_mode()?;
     stdout.execute(cursor::Show)?;
     stdout.execute(LeaveAlternateScreen)?;
 
